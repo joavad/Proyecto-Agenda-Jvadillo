7d80a7a57011b8f1e66422163489d0e833d34b65
Joaquin Vadillo <joaquinvadillo1@gmail.com>
2025-06-24 03:28:55 -0300

    Clases creadas

---
+++ src/proyecto/Main.java
@@ -0,0 +1,10 @@
+package proyecto;
+
+public class Main {
+public static void main(String[] args) {
+    System.out.println();
+    System.out.println();
+    System.out.println("Hola, Agenda de Contactos");
+        // Aquí luego crearás la instancia de Agenda y manejarás el menú
+    }
+}
